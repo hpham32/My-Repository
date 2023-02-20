165f91d3a286720743b8962d6e8810a396867ff9
hpham32 <knightpham93@gmail.com>
2023-02-20 15:51:40 -0500

    Update isFloat.py

--- isFloat.py
+++ isFloat.py
@@ -2,10 +2,7 @@ def isfloat(num):
     try:
         float(num)
         return True
-	      print(“This is a float!”)
     except ValueError:
         return False
-		    print(“Not a float”)
-      
 
 print(isfloat('1.123'))
