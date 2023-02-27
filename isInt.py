605e2f6d20dbb992b5596b7804f2333242e76198
hpham32 <knightpham93@gmail.com>
2023-02-27 10:21:20 -0500

    Update isInt.py

--- isInt.py
+++ isInt.py
@@ -1,10 +1,10 @@
 import re
-regex = r'([0-9] | [[1-9] [0-9]* | [0-7]+ | 0[xX][0-9a-fA-F]+)(i64 | I64 I u | U | l | L)?b'
+regex = "([0-9] | [[1-9] [0-9]* | [0-7]+ | 0[xX][0-9a-fA-F]+)(i64 | I64 I u | U | l | L)?"
 def check(string):
      if(re.fullmatch(regex, string)):
         print("Valid integer constant")
     else:
         print("Invalid integer constant")
 if __name__ == '__main__':
-	string = “anything”
+	string = "anything"
 	check(string)
